7e12d813afa9d9215860a00a4a6e6e9ce61a5080
Jonathan Zheng <jonzheng@mit.edu>
2021-01-12 11:56:44 -0500

    added docstring to calculate_distance

--- molecool/functions.py
+++ molecool/functions.py
@@ -5,15 +5,33 @@ A python package for analyzing and visualizing molecular files. For MolSSI works
 Handles the primary functions
 """
 
-import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D  # noqa : F401
 
 
 def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
+    """
+    Calculate distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    dist : float
+        The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 0.1, 0])
+    >>> calculate_distance(r1,r2)
+    0.1
+    """
     d = rA - rB
     dist = np.linalg.norm(d)
     return dist
@@ -188,7 +206,7 @@ def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
 
-    Replace this function and doc string for your own project
+    Longer explanations (extended summary). Replace this function and doc string for your own project
 
     Parameters
     ----------
