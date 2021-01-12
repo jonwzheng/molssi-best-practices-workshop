7a86da0e7f9d8f98f2eeb285e9a7a66a8906db1f
Jonathan Zheng <jonzheng@mit.edu>
2021-01-12 14:46:56 -0500

    ran black on tests and added multiple angle calc tests

--- molecool/functions.py
+++ molecool/functions.py
@@ -178,7 +178,7 @@ def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
 
     if min_bond < 0:
         raise ValueError("The minimum bond length cannot be less than zero.")
-        
+
     # Find the bonds in a molecule (set of coordinates) based on distance criteria.
     bonds = {}
     num_atoms = len(coordinates)
--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -8,49 +8,76 @@ import pytest
 import sys
 import numpy as np
 
-@pytest.fixture(scope = "module")
-def methane_molecule():
-    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
 
-    coordinates = np.array([
-        [1, 1, 1],
-        [2.4, 1, 1],
-        [-0.4, 1, 1],
-        [1, 1, 2.4],
-        [1, 1, -0.4]
-    ])
+@pytest.fixture(scope="module")
+def methane_molecule():
+    symbols = np.array(["C", "H", "H", "H", "H"])
+
+    coordinates = np.array(
+        [[1, 1, 1], [2.4, 1, 1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]]
+    )
 
     return symbols, coordinates
 
+
+@pytest.mark.parametrize(
+    "p1, p2, p3, expected_value",
+    [
+        (
+            np.array([np.sqrt(2) / 2, np.sqrt(2) / 2, 0]),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            45,
+        ),
+        (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60),
+        (
+            np.array([np.sqrt(3) / 2, (1 / 2), 0]),
+            np.array([0, 0, 0]),
+            np.array([1, 0, 0]),
+            30,
+        ),
+    ],
+)
+def test_calculate_angle_many(p1, p2, p3, expected_value):
+
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+
+    assert expected_value == pytest.approx(
+        calculated_angle
+    ), f"{calculated_angle} {expected_value}"
+
+
 def test_calculate_angle():
-    r1 = np.array([0, 0,-1])
-    r2 = np.array([0, 0, 0]) 
+    r1 = np.array([0, 0, -1])
+    r2 = np.array([0, 0, 0])
     r3 = np.array([1, 0, 0])
 
     expected_value = 90
-    calculated_value =  molecool.calculate_angle(r1, r2, r3, degrees = True)
+    calculated_value = molecool.calculate_angle(r1, r2, r3, degrees=True)
     assert expected_value == calculated_value
 
     expected_value_rad = np.pi / 2
-    calculated_value_rad =  molecool.calculate_angle(r1, r2, r3)
+    calculated_value_rad = molecool.calculate_angle(r1, r2, r3)
     assert expected_value_rad == calculated_value_rad
-    
+
+
 @pytest.mark.skip
 def test_build_bond_list_failure(methane_molecule):
     """ Test that bond list fails as expected with ValueError for negative minbonds"""
-    _, coordinates = methane_molecule 
+    _, coordinates = methane_molecule
 
     with pytest.raises(ValueError):
-        bonds = molecool.build_bond_list(coordinates, min_bond = -1)
+        bonds = molecool.build_bond_list(coordinates, min_bond=-1)
+
 
 def test_build_bond_list(methane_molecule):
     """Test that the bond list forms as expected."""
     _, coordinates = methane_molecule
 
     bonds = molecool.build_bond_list(coordinates)
-    
+
     assert len(bonds) == 4
-    
+
     for bond in bonds.values():
         assert bond == 1.4
 
@@ -60,8 +87,9 @@ def test_calculate_distance():
     r1 = np.array([0, 0, 0])
     r2 = np.array([0, 1, 0])
     expected_distance = 1
-    observed_distance = molecool.calculate_distance(r1,r2)
-    assert  observed_distance == expected_distance
+    observed_distance = molecool.calculate_distance(r1, r2)
+    assert observed_distance == expected_distance
+
 
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
